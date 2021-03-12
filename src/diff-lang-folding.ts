70651da640c40ee7a1474b80fa448df8fe099120
Chris Bain <baincd@users.noreply.github.com>
2021-03-11 23:03:16 -0500

    Update line matches to use regexs based on TextMate grammar

--- src/diff-lang-folding.ts
+++ src/diff-lang-folding.ts
@@ -3,12 +3,21 @@ import * as vscode from 'vscode';
 class DiffFoldingRangeProvider implements vscode.FoldingRangeProvider {
     onDidChangeFoldingRanges?: vscode.Event<void> | undefined;
 
+	// Regexs based on https://github.com/microsoft/vscode/blob/main/extensions/git/syntaxes/diff.tmLanguage.json
+	// private static readonly HeaderGitRegEx = /^diff --git a\/.*$/; // Git RegEx isn't necessary - HeaderCmdRegEx will cover it
+	private readonly headerCmdRegEx = /^diff (-|\S+\s+\S+).*$/
+	private readonly rangeNormalRegEx = /^\d+(,\d+)*(a|d|c)\d+(,\d+)*$/
+	private readonly rangeUnifiedRegEx = /^(@@)\s*(.+?)\s*(@@)/
+	private readonly rangeContextRegEx = /^(((\-{3}) .+ (\-{4}))|((\*{3}) .+ (\*{4})))$/
+
+    
     provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
         let ranges = new Array<vscode.FoldingRange>();
         var previousHeader = -1;
         var previousHunk = -1;
         for (let index = 0; index < document.lineCount && !token.isCancellationRequested; index++) {
-            if (document.lineAt(index).text.match(/^diff --git a\/.*$/)) {
+            let lineText = document.lineAt(index).text;
+            if (lineText.match(this.headerCmdRegEx)) {
                 if (previousHunk >= 0) {
                     ranges.push(new vscode.FoldingRange(previousHunk, index - 1, vscode.FoldingRangeKind.Region));
                     previousHunk = -1;
@@ -19,7 +28,7 @@ class DiffFoldingRangeProvider implements vscode.FoldingRangeProvider {
                     ranges.push(new vscode.FoldingRange(previousHeader, index - 1, vscode.FoldingRangeKind.Region));
                 }
                 previousHeader = index;
-            } else if (document.lineAt(index).text.match(/^@@[ \-+,0-9]*@@.*$/)) {
+            } else if (lineText.match(this.rangeUnifiedRegEx) || lineText.match(this.rangeNormalRegEx) || lineText.match(this.rangeContextRegEx)) {
                 if (previousHunk >= 0) {
                     ranges.push(new vscode.FoldingRange(previousHunk, index - 1, vscode.FoldingRangeKind.Region));
                 } else {
